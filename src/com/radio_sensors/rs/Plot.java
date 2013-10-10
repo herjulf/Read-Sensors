d232657caeb62f09f0533949bbf641d7e1b3300e
Olof Hagsand <olof@hagsand.se>
2013-10-10 13:09:54 +0200

    copyright

--- src/com/radio_sensors/rs/Plot.java
+++ src/com/radio_sensors/rs/Plot.java
@@ -1,3 +1,21 @@
+// Copyright (C) 2013 Olof Hagsand
+//
+// This file is part of Read-Sensors.
+//
+// Read-Sensors is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// Read-Sensors is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Read-Sensors; see the file COPYING.
+
+
 package com.radio_sensors.rs;
 
 import java.util.Set;
