40ebb953ce6f7b1142ac87c563efe64845f671c5
Olof Hagsand <olof@hagsand.se>
2013-10-14 22:17:20 +0200

    New activities

---
+++ src/com/radio_sensors/rs/TextWindow.java
@@ -0,0 +1,112 @@
+// Copyright (C) 2013 Olof Hagsand and Robert Olsson
+//
+// This file is part of Read-Sensors.
+//
+// Read-Sensors is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// Read-Sensors is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Read-Sensors; see the file COPYING.
+
+
+package com.radio_sensors.rs;
+
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Vector;
+import java.util.Random;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Region;
+import android.graphics.Typeface;
+import android.text.format.Time;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.MotionEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
+import android.os.Handler;
+import android.os.Bundle;
+import android.os.Message;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.content.res.Configuration;
+import android.content.Intent;
+import android.view.Display;
+import android.util.Log;
+
+public class TextWindow extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.text);
+    }
+
+    protected void onDestroy(){
+	    super.onDestroy();
+	}
+
+    // This is code for lower right button menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	MenuInflater inflater = getMenuInflater();
+	inflater.inflate(R.layout.text_menu, menu);
+	return true;
+    }	
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+	// Handle item selection
+	switch (item.getItemId()) {
+	case R.id.about:
+	    Toast.makeText(this, "Read-Sensors", 100).show();
+	    return true;
+	case R.id.connect:
+	    return true;
+	case R.id.plot:
+	    toActivity("PlotWindow");
+	    return true;
+	case R.id.prefs:
+	    toActivity("Prefs");
+	    return true;
+	default:
+	    return super.onOptionsItemSelected(item);
+	}
+    }
+
+    private void toActivity(String name){
+	Intent i = new Intent();
+	i.setClassName("com.radio_sensors.rs", "com.radio_sensors.rs."+name);
+	try {
+	    startActivity(i); 
+	}
+	catch (Exception e1){
+	    e1.printStackTrace();
+	}
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+	super.onConfigurationChanged(newConfig);
+	setContentView(R.layout.text);
+    }
+
+}
